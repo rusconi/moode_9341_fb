04f84a8d323963f51be61b03e18a73de78dbbef1
rusconi <59041354+rusconi@users.noreply.github.com>
2026-02-22 10:48:17 +1100

    Update create_service.py

--- create_service.py
+++ create_service.py
@@ -3,7 +3,7 @@ import os
 current_directory = os.getcwd()
 
 first_lines = ["[Unit]\n","Description=Moode 9341 Framebuffer Display\n","After=mpd.socket mpd.service\n","\n","[Service]\n","Type=idle\n","User=root\n"]
-exec_one = "ExecStartPre=/bin/sleep 30\nExecStart=/usr/bin/python3 " + current_directory + "/moode_9341_fb.py\n"
+exec_one = "ExecStart=/usr/bin/python3 " + current_directory + "/moode_9341_fb.py\n"
 last_lines = ["Restart=on-failure\n","\n","[Install]\n","WantedBy=multi-user.target"]
 
 with open('moode9341-fb.service', 'w') as f:
