22a5dc9d9162a2de036b2b78a27e05eeba53aaa5
rusconi <59041354+rusconi@users.noreply.github.com>
2026-02-26 18:13:02 +1100

    correction

--- install-moode-9341-fb.sh
+++ install-moode-9341-fb.sh
@@ -10,6 +10,7 @@ if [[ "$response" == "Y" ]]; then
     sudo cp moode9341-fb.service /lib/systemd/system/
     sudo chmod 644 /lib/systemd/system/moode9341-fb.service
     sudo systemctl daemon-reload
+    sudo systemctl enable moode9341-fb.service
     sudo systemctl start moode9341-fb.service
     sudo systemctl status moode9341-fb.service				
     echo -e "\nmoodetft-fb installed as a service.\n"
